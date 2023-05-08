7bb5ba5d82406f861f056fe88978da7004974045
Petro Silenius <petro.silenius@gmail.com>
2023-05-08 20:39:50 +0300

    Separate nav link to its own component

---
+++ app/NavLink.tsx
@@ -0,0 +1,26 @@
+interface NavLinkProps {
+  children: React.ReactNode;
+  title: string;
+  href: string;
+  external?: boolean;
+}
+
+const NavLink = ({ children, title, href, external }: NavLinkProps) => {
+  return (
+    <a
+      href={href}
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      {...(external && { target: "_blank", rel: "noopener noreferrer" })}
+    >
+      <h2 className={`mb-3 text-2xl font-semibold`}>
+        {title}{" "}
+        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+          -&gt;
+        </span>
+      </h2>
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{children}</p>
+    </a>
+  );
+};
+
+export default NavLink;
